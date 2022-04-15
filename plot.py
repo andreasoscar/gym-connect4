37038df5be586b52b58af3b97a7719eddac133e0
Andreas Olsson <andreasolsson@Andreass-MacBook-Pro.local>
2022-04-15 13:55:28 +0200

    visualisation

--- plot.py
+++ plot.py
@@ -2,9 +2,9 @@
 import matplotlib.pyplot as plt
  
 # x axis values
-x = [6*i for i in range(9)]
+x = [6*i for i in range(10)]
 # corresponding y axis values
-y = [0.52,0.68,0.56,0.56,0.66,0.68,0.7,0.62, 0.76]
+y = [0.52,0.68,0.56,0.56,0.66,0.68,0.7,0.62, 0.76,0.68]
  
 # plotting the points
 plt.plot(x, y)
@@ -14,6 +14,8 @@ ax=plt.gca()
 plt.ylim(ymin=0, ymax=1)
 plt.xlim(xmin=0, xmax=200)
 ax.locator_params('y', nbins=20)
+plt.axhline(y=0.5, color='r', linestyle='-', label='random')
+plt.legend(loc="upper right", borderaxespad=0)
  
 # naming the x axis
 plt.xlabel('MCTS games simulated')
